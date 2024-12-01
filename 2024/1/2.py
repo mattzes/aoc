5b6979c0a790fa181ca22950a6bb69d6c59358bb
Mattes Witthöft <matteswitti@gmail.com>
2024-12-01 22:32:34 +0100

    feat(2024-1): implement input for solution in part 2

--- 2024/1/2.py
+++ 2024/1/2.py
@@ -1,5 +1,14 @@
-left_list = [3, 4, 2, 1, 3, 3]
-right_list = [4, 3, 5, 3, 9, 3]
+with open('./2024/1/input.txt', 'r') as file:
+    left_list = []
+    right_list = []
+    
+    # Iterate through each line
+    for line in file:
+        # Split the line by whitespace
+        left, right = line.split()
+        # Append to respective lists
+        left_list.append(int(left))
+        right_list.append(int(right))
 
 counts = {}
 result = 0
