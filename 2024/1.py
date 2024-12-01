c171a51e4b926b5defc114af03744757a0a24320
Mattes Witthöft <matteswitti@gmail.com>
2024-12-01 21:47:00 +0100

    feat(2024-1): add solution

--- 2024/1.py
+++ 2024/1.py
@@ -1,1 +1,14 @@
-# Start from here
+left_list = [3, 4, 2, 1, 3, 3]
+right_list = [4, 3, 5, 3, 9, 3]
+result = 0
+
+left_list.sort()
+right_list.sort()
+
+for l, r in zip(left_list, right_list):
+    if l > r:
+        result += (l - r)
+    else:
+        result += (r - l)
+
+print(result)
