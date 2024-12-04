14ff694f4f430bc8dcba86ae6b42a040ec691d24
Mattes Witthöft <matteswitti@gmail.com>
2024-12-04 11:39:02 +0100

    feat(2024-4): add solution for part 2

--- 2024/4/2.py
+++ 2024/4/2.py
@@ -8,41 +8,23 @@ with open('./2024/4/input.txt', 'r') as file:
 
 Y_RANGE = len(word_search)
 X_RANGE = len(word_search[0])
-DIRECTIONS = [
-    (0, 1), # to right
-    (1, 1), # to lower right
-    (1, 0), # to lower
-    (1, -1), # to lower left
-    (0, -1), # to left
-    (-1, -1), # to upper left
-    (-1, 0), # to upper
-    (-1, 1) # to upper right
-]
-WORD_TO_FIND = 'XMAS'
 
-def count_xmas_directions(y, x):
-    count = 0
-    for direction in DIRECTIONS:
-        for i in range(1, len(WORD_TO_FIND)):
-            new_x = x + direction[1] * i
-            new_y = y + direction[0] * i
-            if new_y < 0 or new_y >= Y_RANGE or new_x < 0 or new_x >= X_RANGE:
-                # break, because we are out of bounds
-                break
-            if word_search[new_y][new_x] != WORD_TO_FIND[i]:
-                # break, because the letter is not the one we are looking for
-                break
-            if i == 3:
-                count += 1
-    return count
+def is_a_cross(y: int, x: int) -> bool:
+    is_a_cross = 0
+    for dy, dx in [(-1, -1), (-1, 1)]:
+        upper_y, upper_x = y + dy, x + dx
+        lower_y, lower_x = y - dy, x - dx
+        if (word_search[upper_y][upper_x], word_search[lower_y][lower_x]) in {("M", "S"), ("S", "M")}:
+            is_a_cross += 1
+    return is_a_cross == 2
 
 def count_xmas():
     count = 0
-    for y in range(len(word_search)):
-        for x in range(len(word_search[y])):
-            if word_search[y][x] == WORD_TO_FIND[0]:
-                count += count_xmas_directions(y, x)
-                
+    for y in range(1, len(word_search) - 1):
+        for x in range(1, len(word_search[y]) - 1):
+            if word_search[y][x] == "A" and is_a_cross(y, x):
+                count += 1
+
     return count
 
 print(count_xmas())
