62055bdec15493cfcb754bb90cf3e2bc05757bc0
Mattes Witthöft <matteswitti@gmail.com>
2024-12-05 19:18:55 +0100

    feat(2024-5): add solution for part 2 but it isnt good as i want it to be

--- 2024/5/2.py
+++ 2024/5/2.py
@@ -4,32 +4,70 @@ with open('./2024/5/input.txt', 'r') as file:
 split_index = input.index('')
 updates = [update.split(",") for update in input[split_index + 1:]]
 
-orders_x = {}
-orders_y = {}
+allowed_right = {}
+allowed_left = {}
 for x, y in (order.split("|") for order in input[:split_index]):
-    orders_x.setdefault(x, []).append(y)
-    orders_y.setdefault(y, []).append(x)
+    allowed_right.setdefault(x, []).append(y)
+    allowed_left.setdefault(y, []).append(x)
 
-def check_order(page, left_pages, right_pages) -> bool:
-    if page in orders_x.keys() and any(left_page in orders_x[page] for left_page in left_pages):
-        return False
-    if page in orders_y.keys() and any(right_page in orders_y[page] for right_page in right_pages):
-        return False
+def is_order_valid_to_left(page, left_pages) -> bool:
+    if page in allowed_right.keys():
+        for left_page in left_pages:
+            if left_page in allowed_right[page]:
+                # print("left") # issue is on the right site
+                return False
     return True
 
-def is_update_valid(update) -> bool:
+def is_order_valid_to_right(page, right_pages) -> bool:
+    if page in allowed_left.keys():
+        for right_page in right_pages:
+            if right_page in allowed_left[page]:  
+                # print("right") # issue is on the left site
+                return False
+    return True
+
+def is_update_invalid(update) -> tuple:
     for i, page in enumerate(update):
         left_pages = update[:i]
         right_pages = update[i + 1:]
-        if not check_order(page, left_pages, right_pages):
-            return False
-    return True
+        if not is_order_valid_to_left(page, left_pages):
+            return i, "left"
+        if not is_order_valid_to_right(page, right_pages):
+            return i, "right"
+    return False, ""
+
+def get_new_index(update, index_of_reordering_page, direction) -> int:
+    if direction == "right":
+        index = 0
+        for page in update[index_of_reordering_page + 1:]:
+            if page in allowed_left[update[index_of_reordering_page]]:
+                index = update.index(page)
+        return index
+    if direction == "left":
+        index = len(update)
+        for page in update[:index_of_reordering_page]:
+            if page in allowed_right[update[index_of_reordering_page]]:
+                index = update.index(page)
+        return index
+
+def order_pages(update: list, index_of_reordering_page: int, direction: str) -> list:
+    ordered_pages = update.copy()
+    new_index = get_new_index(update, index_of_reordering_page, direction)
+    page_to_reorder = ordered_pages.pop(index_of_reordering_page)
+    ordered_pages.insert(new_index, page_to_reorder)
+    return ordered_pages
 
 def cehck_updates():
     page_sum = 0
     for update in updates:
-        if is_update_valid(update):
-            page_sum += int(update[len(update) // 2])
+        reordered_update = update.copy()
+        invalid, direction = is_update_invalid(update)
+        while type(invalid) == int:
+            reordered_update = order_pages(reordered_update, invalid, direction)
+            invalid, direction = is_update_invalid(reordered_update)
+            if type(invalid) == bool:
+                page_sum += int(reordered_update[len(reordered_update) // 2])
+            
     
     return page_sum
 
