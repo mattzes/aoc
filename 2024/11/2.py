50193b387709920af9ad8f2fd6596ebbd45e7d35
Mattes Witthöft <matteswitti@gmail.com>
2024-12-12 23:37:46 +0100

    feat(2024-11): add solution for part 2

--- 2024/11/2.py
+++ 2024/11/2.py
@@ -1,31 +1,37 @@
 with open('./2024/11/input.txt', 'r') as file:
     stones = file.read().split(" ")
 
-BLINKS = 25
+BLINKS = 75
+cache = {}
 
-def blink(stones: list[str], blinks_left: int = BLINKS) -> int:
-    print(blinks_left)
+def blink(stone: str, blinks_left: int = BLINKS) -> int:
+    if (stone, blinks_left) in cache:
+        return cache[(stone, blinks_left)]    
+    
     count_stones = 0
-    for stone in stones:
-        if blinks_left == 0:
-            count_stones += 1
-            continue
-        
-        # rule number 1
-        if stone == "0":
-            count_stones += blink(["1"], blinks_left - 1)
-            continue
-        
-        # rule number 2
-        if len(stone) % 2 == 0:
-            first_stone, second_stone = stone[:int(len(stone)/2)], stone[int(len(stone)/2):]
-            second_stone = second_stone.lstrip("0")
-            second_stone = second_stone if second_stone else "0"
-            count_stones += blink([first_stone, second_stone], blinks_left - 1)
-            continue
+    if blinks_left == 0:
+        count_stones += 1
+    
+    # rule number 1
+    elif stone == "0":
+        count_stones += blink("1", blinks_left - 1)
+    
+    # rule number 2
+    elif len(stone) % 2 == 0:
+        first_stone, second_stone = stone[:int(len(stone)/2)], stone[int(len(stone)/2):]
+        second_stone = second_stone.lstrip("0")
+        second_stone = second_stone if second_stone else "0"
+        count_stones += blink(first_stone, blinks_left - 1)
+        count_stones += blink(second_stone, blinks_left - 1)
+    else:
         new_stone = str(int(stone) * 2024)
-        count_stones += blink([new_stone], blinks_left - 1)
+        count_stones += blink(new_stone, blinks_left - 1)
+    
+    cache[(stone, blinks_left)] = count_stones
+    
     return count_stones
         
-
-print(blink(stones))
+stone_count = 0
+for stone in stones:
+    stone_count += blink(stone)
+print(stone_count)
