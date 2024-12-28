c9f4a3b370932049ab7754797958ca43cf18206c
Mattes Witthöft <matteswitti@gmail.com>
2024-12-28 13:46:46 +0100

    feat(2024-14): add uncommon solution for part 2

--- 2024/14/2.py
+++ 2024/14/2.py
@@ -1,7 +1,9 @@
+import time
+
 with open('./2024/14/input.txt', 'r') as file:
     raw_robots = file.read().splitlines()
 
-STEPS = 100
+STEPS = 10000
 X_LENGTH = 101
 Y_LENGTH = 103
 
@@ -12,30 +14,25 @@ class Robot():
         velocity = velocity.split('=')[-1].split(',')
         self.positon = (int(positon[0]), int(positon[1]))
         self.velocity = (int(velocity[0]), int(velocity[1]))
+
+    def step(self):
+        self.positon = ((self.positon[0] + self.velocity[0]) % X_LENGTH, (self.positon[1] + self.velocity[1]) % Y_LENGTH)
+
+robots = [Robot(raw_robot) for raw_robot in raw_robots]
+
+for i in range(STEPS):
+    co = [['.' for _ in range(X_LENGTH)] for _ in range(Y_LENGTH)]
+
+    print(i + 1)
     
-    def move(self):
-        x = (self.positon[0] + self.velocity[0] * STEPS) % X_LENGTH
-        y = (self.positon[1] + self.velocity[1] * STEPS) % Y_LENGTH
-        self.positon = (x, y)
-
-Q1 = ((0, 0), (X_LENGTH // 2 - 1, Y_LENGTH // 2 - 1))
-Q2 = ((X_LENGTH // 2 + 1, 0), (X_LENGTH - 1, Y_LENGTH // 2 - 1))
-Q3 = ((0, Y_LENGTH // 2 + 1), (X_LENGTH // 2 - 1, Y_LENGTH - 1))
-Q4 = ((X_LENGTH // 2 + 1, Y_LENGTH // 2 + 1), (X_LENGTH - 1, Y_LENGTH - 1))
-
-quadrents = [Q1, Q2, Q3, Q4]
-quadrents_count = [0, 0, 0, 0]
-
-robots = []
-for raw_robot in raw_robots:
-    robot = Robot(raw_robot)
-    robots.append(robot)
+    for robot in robots:
+        robot.step()
+        co[robot.positon[1]][robot.positon[0]] = '#'
     
-    robot.move()
+    for line in co:
+        count = line.count('#')
+        if count > 25:
+            for l in co:
+                print(''.join(l))
 
-    for i, Q in enumerate(quadrents):
-        if Q[0][0] <= robot.positon[0] <= Q[1][0] and Q[0][1] <= robot.positon[1] <= Q[1][1]:
-            quadrents_count[i] += 1
-            break
-
-print(quadrents_count[0] * quadrents_count[1] * quadrents_count[2] * quadrents_count[3])
+    time.sleep(0.02)
